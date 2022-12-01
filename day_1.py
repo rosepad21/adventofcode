8051f6af383e44289f3e874b82b4fea839dc2891
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 11:07:17 +0100

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,14 +1,16 @@
 def calories_count(x):
     fh = open(x)
-    new = []
+    elves_calories = [] # list containing all the calories every elf is carrying
     count = 0
     for line in fh:
         line = line.strip()
         if line.isdigit():
+            # if the line contains a digit, we add it to the count variable
             count += int(line)
         else:
+            # else, it means we counted all the calories for that elf
+            # we append the total to the list and we reset the count
             new.append(count)
             count = 0
+    # returning the max value from the list
     return max(new)
-
-print(calories_count('input.txt'))
