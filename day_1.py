e7cc5c36d3dcd5680b00bb67524a087d7066b252
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 11:21:58 +0100

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,16 +1,19 @@
-def calories_count(x):
-    fh = open(x)
-    elves_calories = [] # list containing all the calories every elf is carrying
-    count = 0
-    for line in fh:
-        line = line.strip()
-        if line.isdigit():
-            # if the line contains a digit, we add it to the count variable
-            count += int(line)
-        else:
-            # else, it means we counted all the calories for that elf
-            # we append the total to the list and we reset the count
-            new.append(count)
-            count = 0
-    # returning the max value from the list
-    return max(new)
+fh = open('input.txt)
+elves_calories = [] # list containing all the calories every elf is carrying
+count = 0
+for line in fh:
+    line = line.strip()
+    if line.isdigit():
+        # if the line contains a digit, we add it to the count variable
+        count += int(line)
+    else:
+        # else, it means we counted all the calories for that elf
+        # we append the total to the list and we reset the count
+        new.append(count)
+        count = 0
+
+max_calories = max(new)
+          
+# now on to find the sum of the top three elves' calories
+sorted_list = sorted(elves_calories, reverse=True)
+top_three = sum(sorted_list[:3])
