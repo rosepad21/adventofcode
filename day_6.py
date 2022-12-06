2aedab2890823324b5e6d715bb1d5c29730b4d33
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-06 10:28:41 +0100

    Update day_6.py

--- day_6.py
+++ day_6.py
@@ -1,6 +1,6 @@
 input_file = 'input.txt'
 
-def find_marker(input_file, first_half_state=False):
+def find_marker(input_file, n):
     fh = open(input_file)
     marker = []
     count = 0
@@ -9,28 +9,18 @@ def find_marker(input_file, first_half_state=False):
         for i in line:
             marker.append(i)
             count += 1
-            if first_half_state:
-                if len(marker) == 4:
-                    if len(marker) == len(set(marker)): # comparing it to a set to check for duplicates
-                        break
-                    else:
-                        marker.pop(0) # if there are duplicates, we remove the first element in the list
-                        continue
+            if len(marker) == n:
+                if len(marker) == len(set(marker)):
+                    break
                 else:
+                    marker.pop(0)
                     continue
             else:
-                if len(marker) == 14:
-                    if len(marker) == len(set(marker)):
-                        break
-                    else:
-                        marker.pop(0)
-                        continue
-                else:
-                    continue
+                continue
                 
 
     return count
   
-  first_half_solution = find_marker(input_file, True)
-  second_half_solution = find_marker(input_file)
+  first_half_solution = find_marker(input_file, 4)
+  second_half_solution = find_marker(input_file, 14)
   print(f'The solution to the first half of the problem is {first_half_solution}, while the solution to the second half is {second_half_solution}.')
